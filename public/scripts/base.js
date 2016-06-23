781f17ae6fd4583122ff9fb7926ce83b21af6b9a
Marina Sheenkova <msheenkova@gmail.com>
2016-06-23 14:20:33 +0300

    finished stage with redux(not work)

--- public/scripts/base.js
+++ public/scripts/base.js
@@ -1,147 +1,203 @@
+//import { connect } from 'react-redux'
+//-----------------------------------------------------------
+// Actions
+//-----------------------------------------------------------
+
 const ADD_ADDRESS = 'ADD_ADDRESS';
 const DEL_ADDRESS = 'DEL_ADDRESS';
 const REFRESH_ADDRESS= 'REFRESH_ADDRESS';
 
+let nextTodoId = 0;
+
 function addAddress(text) {
   return {
     type: ADD_ADDRESS,
+    id: nextTodoId++,
     text
   }
 }
 
-function refreshAddress(index) {
-  return { type: REFRESH_ADDRESS, index }
+function refreshAddress(id) {
+  return { type: REFRESH_ADDRESS, id }
 }
 
-function address (state = [], action) {
+//----------------------------------------------------------
+//Reducers
+//----------------------------------------------------------
+var address = (state, action) => {
     switch (action.type) {
         case ADD_ADDRESS:
-            return [
-                ...state,
-                {
-                    text: action.text,
-                    info: {"coord":{"lon":-122.09,"lat":37.39},
-                            "sys":{"type":3,"id":168940,"message":0.0297,"country":"US","sunrise":1427723751,"sunset":1427768967},
-                            "weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}],
-                            "base":"stations",
-                            "main":{"temp":285.68,"humidity":74,"pressure":1016.8,"temp_min":284.82,"temp_max":286.48},
-                            "wind":{"speed":0.96,"deg":285.001},
-                            "clouds":{"all":0},
-                            "dt":1427700245,
-                            "id":0,
-                            "name":"Mountain View",
-                            "cod":200}
-                }
-            ];
+            return {
+                id: action.id,
+                text: action.text,
+                info: {"coord":{"lon":-122.09,"lat":37.39},
+                    "sys":{"type":3,"id":168940,"message":0.0297,"country":"US","sunrise":1427723751,"sunset":1427768967},
+                    "weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}],
+                    "base":"stations",
+                    "main":{"temp":285.68,"humidity":74,"pressure":1016.8,"temp_min":284.82,"temp_max":286.48},
+                    "wind":{"speed":0.96,"deg":285.001},
+                    "clouds":{"all":0},
+                    "dt":1427700245,
+                    "id":0,
+                    "name":"Mountain View",
+                    "cod":200}
+            };
         case REFRESH_ADDRESS:
-            return state.map((state, index) => {
-                if (index === action.index) {
-                    return Object.assign({}, address, {
-                        info: {"coord":{"lon":-122.09,"lat":37.39},
-                            "sys":{"type":3,"id":168940,"message":0.0297,"country":"US","sunrise":1427723751,"sunset":1427768967},
-                            "weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}],
-                            "base":"stations",
-                            "main":{"temp":285.68,"humidity":74,"pressure":1016.8,"temp_min":284.82,"temp_max":286.48},
-                            "wind":{"speed":0.96,"deg":285.001},
-                            "clouds":{"all":0},
-                            "dt":1427700245,
-                            "id":0,
-                            "name":"Mountain View",
-                            "cod":200}
-                    })
-                }
-                return address
-            });
+
+            if (state.id === action.id) {
+                return Object.assign({}, state, {
+                    info: {"coord":{"lon":-122.09,"lat":37.39},
+                        "sys":{"type":3,"id":168940,"message":0.0297,"country":"US","sunrise":1427723751,"sunset":1427768967},
+                        "weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}],
+                        "base":"stations",
+                        "main":{"temp":285.68,"humidity":74,"pressure":1016.8,"temp_min":284.82,"temp_max":286.48},
+                        "wind":{"speed":0.96,"deg":285.001},
+                        "clouds":{"all":0},
+                        "dt":1427700245,
+                        "id":0,
+                        "name":"Mountain View",
+                        "cod":200}
+                })
+            }
+            return state;
+
         default:
             return state
     }
-}
-
-const addressApp = {address};
+};
 
 
+const addresses = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_ADDRESS':
+            return [
+                ...state,
+                address(undefined, action)
+            ];
+        case 'REFRESH_ADDRESS':
+            return state.forEach(t =>
+                address(t, action)
+            );
+        default:
+            return state
+    }
+};
+
+// const addressApp = (state = {}, action) => {
+//     addresses: addresses(state.addresses, action)
+// };
+let store = Redux.createStore(addresses);
 
 
+//---------------------------------------------------------------------
+//Components
+//----------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-const Map = ({address}) => (
+const Map = ({info}) => (
     <div className="map">
-        {address}
+        {info.coord.lon}<br/>
+        {info.coord.lat}
     </div>
 );
 
 
 
-const Weather = ({address}) => (
+const Weather = ({info}) => (
     <div className="weather">
-        <span>Temp</span><br/>
-        <span>Temp</span><br/>
-        <span>Temp</span>
+        <span>{info.main.temp}</span><br/>
+        <span>{info.wind.speed}</span><br/>
+        <span>{info.weather.main}</span>
     </div>
 );
 
 
 
-const LocationBlock  = ({address}) => (
+const LocationBlock  = ({address, onRefresh}) => (
     <div className="locationBlock">
-                <Map address={address}/>
-                <Weather address={address}/>
-                <i className="fa fa-refresh">Refresh</i>
+                <Map info={address.info}/>
+                <Weather info={address.info}/>
+                <i className="fa fa-refresh" onClich={onRefresh}>Refresh</i>
                 <i className="fa fa-trash-o">Delete</i>
             </div>
 );
 
 
-const List  = ({address}) => (
-    <ul className="list">
-        <LocationBlock address={address} />
 
-    </ul>
-
+const List = ({ addresses, onRefreshClick }) => (
+  <ul className="list">
+    {addresses.forEach(address =>
+      <LocationBlock
+        key={address.id}
+        {...address}
+        onRefresh={() => onRefreshClick(address.id)}
+      />
+    )}
+  </ul>
 );
 
-
-
-const AddressForm  = ({onClick}) => (
-     <form  className="addressForm">
-         <input type="text" placeholder="Address"></input>
-         <button type="submit">+</button>
-     </form>
-    // <form className="addressForm">
-    //     <FormGroup controlId="formControlsText">
-    //         <ControlLabel>Text</ControlLabel>
-    //         <FormControl type="text" placeholder="Enter address" />
-    //     </FormGroup>
-    //     <Button type="submit">
-    //         Submit
-    //     </Button>
-    // </form>
-);
-
-var foo = function (){
-    console.log('foo');
-};
-
-
-const Page  = ({data}) => (
+const Page  = () => (
     <div className="page">
-        <AddressForm onClick={foo}/>
-        <List address='Tel-aviv'/>
+        <AddAddress />
+        <VisibleList />
     </div>
 
 );
 
+//-----------------------------------------------------------------------
+//Containers
+//-----------------------------------------------------------------------
+
+const mapStateToProps = (state) => {
+  return {
+    addresses: state.addresses
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onRefreshClick: (id) => {
+      dispatch(refreshAddress(id))
+    }
+  }
+};
+
+const VisibleList = ReactRedux.connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(List);
+
+
+let AddAddress = ({ dispatch }) => {
+  let input;
+
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault();
+        if (!input.value.trim()) {
+          return
+        }
+        dispatch(addAddress(input.value));
+        input.value = '';
+      }}>
+        <input ref={node => {
+          input = node
+        }} />
+        <button type="submit">
+          Add
+        </button>
+      </form>
+    </div>
+  )
+};
+AddAddress = ReactRedux.connect()(AddAddress);
+
+
+
 
 ReactDOM.render(
-    <Page />,
+    <ReactRedux.Provider store={store}>
+        <Page />
+    </ReactRedux.Provider>,
     document.getElementById('content')
 );
