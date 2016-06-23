2cc9a1db5cd42b5f38a9e75aeef016da0122de24
Marina Sheenkova <msheenkova@gmail.com>
2016-06-23 15:44:17 +0300

    fix info

--- public/scripts/base.js
+++ public/scripts/base.js
@@ -7,12 +7,12 @@ const ADD_ADDRESS = 'ADD_ADDRESS';
 const DEL_ADDRESS = 'DEL_ADDRESS';
 const REFRESH_ADDRESS= 'REFRESH_ADDRESS';
 
-let nextTodoId = 0;
+let nextId = 0;
 
 function addAddress(text) {
     return {
         type: ADD_ADDRESS,
-        id: nextTodoId++,
+        id: nextId++,
         text
     }
 }
@@ -26,23 +26,15 @@ function refreshAddress(id) {
 //----------------------------------------------------------
 var address = (state, action) => {
     switch (action.type) {
-        case ADD_ADDRESS:
+        case 'ADD_ADDRESS':
             return {
                 id: action.id,
                 text: action.text,
-                info: {"coord":{"lon":-122.09,"lat":37.39},
-                    "sys":{"type":3,"id":168940,"message":0.0297,"country":"US","sunrise":1427723751,"sunset":1427768967},
-                    "weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}],
-                    "base":"stations",
-                    "main":{"temp":285.68,"humidity":74,"pressure":1016.8,"temp_min":284.82,"temp_max":286.48},
-                    "wind":{"speed":0.96,"deg":285.001},
-                    "clouds":{"all":0},
-                    "dt":1427700245,
-                    "id":0,
-                    "name":"Mountain View",
-                    "cod":200}
+                info: {coord: {lon: 12, lat: 12}, weather: [{main: "Clear"}],
+                        main:{temp: 30}, wnd: {speed: 0.89}}
+
             };
-        case REFRESH_ADDRESS:
+        case 'REFRESH_ADDRESS':
 
             if (state.id === action.id) {
                 return Object.assign({}, state, {
@@ -106,19 +98,22 @@ const Weather = ({info}) => (
     <div className="weather">
         <span>{info.main.temp}</span><br/>
         <span>{info.wind.speed}</span><br/>
-        <span>{info.weather.main}</span>
+        <span>{info.weather[0].main}</span>
     </div>
 );
 
 
 
-const LocationBlock  = ({address, onRefresh}) => (
+const LocationBlock  = ({info, onRefresh}) => (
     <li>
-        <div className="locationBlock">
-            <Map info={address.info}/>
-            <Weather info={address.info}/>
-            <i className="fa fa-refresh" onClich={onRefresh}>Refresh</i>
-            <i className="fa fa-trash-o">Delete</i>
+        <div className="locationBlock container">
+            <Map info={info}/>
+            <Weather info={info}/>
+            <a href="#" onClick={e => {
+                 e.preventDefault()
+                 onRefresh()
+               }}><i className="fa fa-refresh"></i></a>
+            <i className="fa fa-trash-o"></i>
         </div>
     </li>
 );
@@ -167,18 +162,19 @@ let AddAddress = ({ dispatch }) => {
     let input;
 
     return (
-        <div>
+        <div className="container">
             <form onSubmit={e => {
-        e.preventDefault();
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addAddress(input.value));
-        input.value = '';
-      }}>
+                e.preventDefault();
+                console.log(!input.value.trim());
+                if (!input.value.trim()) {
+                  return
+                }
+                dispatch(addAddress(input.value));
+                input.value = '';
+              }}>
                 <input ref={node => {
-          input = node
-        }} />
+                  input = node
+                }} />
                 <button type="submit">
                     Add
                 </button>
